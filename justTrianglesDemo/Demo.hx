1bef59181af51353c46fda60c3c4d59f31b7cbad
nanjizal <nitsuj@nanjizal.net>
2017-01-08 16:21:57 +0000

    Update Demo.hx

--- justTrianglesDemo/Demo.hx
+++ justTrianglesDemo/Demo.hx
@@ -32,9 +32,28 @@ class Demo {
     }
     public function draw(){
         Draw.colorFill_id = 1; 
-        Draw.colorLine_id = 1;
-        Draw.extraFill_id = 2;
-        Draw.thickness = 25/1000;
+        Draw.colorLine_id = 3;
+        Draw.extraFill_id = 1;
+        var ctx = new PathContext( 1, 200 );
+        ctx.moveTo(75,40);
+        ctx.curveTo(75,37,70,25,50,25);
+        ctx.curveTo(20,25,20,62.5,20,62.5);
+        ctx.curveTo(20,80,40,102,75,120);
+        ctx.curveTo(110,102,130,80,130,62.5);
+        ctx.curveTo(130,62.5,130,25,100,25);
+        ctx.curveTo(85,25,75,37,75,40);
+        ctx.render( 1);
+        ctx.clear();
+        ctx = new PathContext( 1, 200, 200, 100 );
+        ctx.moveTo(75,25);
+        ctx.quadTo(25,25,25,62.5);
+        ctx.quadTo(25,100,50,100);
+        ctx.quadTo(50,120,30,125);
+        ctx.quadTo(60,120,65,100);
+        ctx.quadTo(125,100,125,62.5);
+        ctx.quadTo(125,25,75,25);
+        ctx.render( 1 ); 
+        var thick: Float = (Math.random()*20)/800;
         var rndEnds = [ true, false ];
         for( i in 0...12 ){
             var aCol =  Math.round( Math.random()*(rainbow.length-1));
@@ -42,7 +61,30 @@ class Demo {
             Draw.colorLine_id = aCol;
             Draw.extraFill_id = aCol;
             var roundEnd = rndEnds[ Math.round( Math.random() ) ];
-            Draw.isolatedLine( 0, { x: (300 + Math.random()*800)/800 - 0.5, y: ( -200 + Math.random()*800 )/800 }, { x: ( 300 + Math.random()*800 )/800 - 0.5,y: (-200 + Math.random()*800)/800 }, ( 10+Math.random()*20 )/800, roundEnd  );
-        }   
+            thick = ( Math.random()*20 )/800;
+            Draw.isolatedLine( 0, { x: (300 + Math.random()*800)/800 - 0.5, y: ( -200 + Math.random()*800 )/800 }, { x: ( 300 + Math.random()*800 )/800 - 0.5,y: (-200 + Math.random()*800)/800 }, thick, roundEnd  );
+        }
+        var thick = Math.random()*20;
+        var ctx = new PathContext( 1, 200 );
+        ctx.moveTo(75,40);
+        ctx.curveTo(75,37,70,25,50,25);
+        ctx.curveTo(20,25,20,62.5,20,62.5);
+        ctx.curveTo(20,80,40,102,75,120);
+        ctx.curveTo(110,102,130,80,130,62.5);
+        ctx.curveTo(130,62.5,130,25,100,25);
+        ctx.curveTo(85,25,75,37,75,40);
+        ctx.render( thick, false );
+        ctx.clear();
+        ctx = new PathContext( 1, 200, 200, 100 );
+        ctx.moveTo(75,25);
+        ctx.quadTo(25,25,25,62.5);
+        ctx.quadTo(25,100,50,100);
+        ctx.quadTo(50,120,30,125);
+        ctx.quadTo(60,120,65,100);
+        ctx.quadTo(125,100,125,62.5);
+        ctx.quadTo(125,25,75,25);
+        ctx.regularPoly( PathContext.circleSides, 100, 100, 100, 0 );
+        //ctx.roundedRectangle( 260, 400, 150, 60, 20, 14 );
+        ctx.render( thick, false );  
     }
 }
